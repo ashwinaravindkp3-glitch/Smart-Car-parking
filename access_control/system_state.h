9303b972d101ef3f4e773507680cc21b923702fd
ashwinaravindkp3-glitch <ashwinaravindkp3@gmail.com>
2025-11-07 10:21:29 +0530

    Delete access_control directory

--- access_control/system_state.h
+++
@@ -1,8 +0,0 @@
-#ifndef SYSTEM_STATE_H
-#define SYSTEM_STATE_H
-
-// A global flag to communicate between modules.
-// 'extern' tells the compiler this variable is defined in another file.
-extern bool userJustValidated;
-
-#endif
